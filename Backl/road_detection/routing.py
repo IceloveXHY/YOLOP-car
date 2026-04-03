e16946233d31be99f2943305dcadbb05d0e80a54
LoveXHY <137680776+IceloveXHY@users.noreply.github.com>
2026-04-03 11:09:45 +0800

    Delete Backl directory

--- Backl/road_detection/routing.py
+++
@@ -1,7 +0,0 @@
-# road_detection/routing.py
-from django.urls import re_path
-from road_detection import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/process/(?P<task_id>[\w-]+)/$', consumers.VideoProcessingConsumer.as_asgi()),
-]
